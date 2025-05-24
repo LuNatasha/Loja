6af491fbf7ed1a3850685a77348e9eb7eb9933f6
Luiza Natasha <luizanatashaxd@gmail.com>
2025-05-24 20:25:42 +0000

    Entidade Catergoria

---
+++ loja/src/main/java/com/example/loja/model/Categoria.java
@@ -0,0 +1,39 @@
+package com.example.loja;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Catergoria{
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    public Categoria() {
+    }
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
